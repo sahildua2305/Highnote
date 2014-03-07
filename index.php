15734b44666cdfa6d84b8e290e4e814628c1bdc1
Sahil <sahildua2305@gmail.com>
2014-03-07 18:01:59 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sample Wiki page</title>
+	
+	
+	<script language="JavaScript" type="text/javascript" src="js/jquery-1.10.2.js"></script>
+	<script language="JavaScript" type="text/javascript" src="js/main.js"></script>
+	<link rel="stylesheet" href="css/style.css"/>
+	
+</head>
+<body>
+	<h1>Snapchat</h1>
+	<p>Snapchat is a photo messaging application ("app") developed by Reggie Brown, Evan Spiegel, and Robert Murphy, then Stanford University students.[3][4][5] Using the app, users can take photos, record videos, add text and drawings, and send them to a controlled list of recipients. These sent photographs and videos are known as "Snaps". Users set a time limit for how long recipients can view their Snaps (as of December 2013, the range is from 1 to 10 seconds),[6] after which they will be hidden from the recipient's device and deleted from Snapchat's servers.</p>
+	<p>As of July 2013, the interface of Snapchat, on both the Android and iOS versions, consists of a large circular button located at the center of the bottom portion of the screen, flanked on the sides by a picture of a three-dimensional box on the left hand side, and a two-dimensional striped box on the right hand side. Snapchat versions 1.0 to 4.0 originally had a blue coat covering these three buttons, although the blue coat was discarded in version 5.0.
+
+	The large circular button is the camera button. By pressing the button once, the application will take a still image. The user can then proceed to alter the image, by applying text and/or ink drawings onto the image, before sending it off to a controlled list of recipients. By holding the button, the application will instead record a short video lasting up to 10 seconds which, just like a still image, can have text applied to it by the user, before it is sent away. Recorded images and videos can be saved by the sender prior to sending by pressing the white arrow in the bottom left-hand corner.
+
+	The two-dimensional striped box on the right hand side of the interface takes the user to their contacts list, allowing the user to view their Snapchat friends and find friends from their phone contacts. Previously on Snapchat versions 1.0 to 4.0, this button served as an options menu. Pressing this box will bring up a three selection menu, allowing the user to either view their friends, find friends from their phone contacts, or to adjust the settings of the application, on matters such as notification settings, and who can send the user Snaps.
+
+	The three-dimensional box on the left hand side of the interface takes the Snapchat user to a menu that shows the user the snaps he/she has been sent by other Snapchat users, along with the snaps that the user has sent to other users. Users are informed by text whenever one of their snaps has been viewed or not, and whether it has been screenshotted or not. Users view Snaps sent to them by other Snapchat users by pressing and holding onto the image or video. The image or video will remain for the time set before it is permanently removed. As of Version 5.0, the Snapchat options menu is accessed by accessing the Snaps menu, and then pressing the cog on the top right hand corner of the Snaps menu.</p>
+</body>
+</html>
